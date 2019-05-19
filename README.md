c2571a05c199d7c4f684b365af50f882d2aed663
tcly861204 <tcly861204@hotmail.com>
2019-05-20 07:40:12 +0800

    merge

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 
-# blog
-个人创作
+# 博客
 
 [![Build Status](https://www.travis-ci.org/tcly861204/blog.svg?branch=master)](https://www.travis-ci.org/tcly861204/blog)
 
 ## 学习笔记
 
 
-[javascript设计模式之单例模式](https://tcly861204.github.io/2019/05/11/javascript设计模式之单例模式/)
-[javascript设计模式之适配器模式](https://tcly861204.github.io/2019/05/12/javascript设计模式之适配器模式/)
-[javscript高阶函数浅析](https://tcly861204.github.io/2019/05/12/javscript高阶函数浅析/)
-[koa构建大型项目](https://tcly861204.github.io/2019/05/14/koa构建大型项目/)
-[vue组件间通信六种方式](https://tcly861204.github.io/2019/05/19/vue组件间通信六种方式/)
+1. [javascript设计模式之单例模式](https://tcly861204.github.io/2019/05/11/javascript设计模式之单例模式/)
+2. [javascript设计模式之适配器模式](https://tcly861204.github.io/2019/05/12/javascript设计模式之适配器模式/)
+3. [javscript高阶函数浅析](https://tcly861204.github.io/2019/05/12/javscript高阶函数浅析/)
+4. [koa构建大型项目](https://tcly861204.github.io/2019/05/14/koa构建大型项目/)
+5. [vue组件间通信六种方式](https://tcly861204.github.io/2019/05/19/vue组件间通信六种方式/)
--- cread/node.js
+++ cread/node.js
@@ -3,8 +3,7 @@ const path = require('path');
 const rootPath = path.resolve(__dirname, '..');
 const filePath = path.resolve(__dirname, '../source/_posts');
 let str = `
-# blog
-个人创作
+# 博客
 
 [![Build Status](https://www.travis-ci.org/tcly861204/blog.svg?branch=master)](https://www.travis-ci.org/tcly861204/blog)
 
@@ -13,23 +12,26 @@ let str = `
 
 `
 const files = fs.readdirSync(filePath);
-files.forEach(function(filename, _index){
+function readFile(files, index) {
+  let filename = files[index]
   let filedir = path.join(filePath, filename);
   fs.stat(filedir,function(eror, stats){
     if (eror) {
       console.warn('获取文件stats失败');
     } else {
       const isFile = stats.isFile();//是文件
-      // const isDir = stats.isDirectory();//是文件夹
       if (isFile) {
         let content = fs.readFileSync(filedir, 'utf-8');
         let startIndexOf = content.indexOf('date: ')
         if (startIndexOf > 0) {
           let date = content.substr(startIndexOf + 6, 10)
           let name = filename.replace('.md', '')
-          str += '[' + name + '](https://tcly861204.github.io/' + date.split('-').join('/') + '/' + name + '/)\r\n'
+          str += (index + 1) + '. [' + name + '](https://tcly861204.github.io/' + date.split('-').join('/') + '/' + name + '/)\r\n'
         }
-        if (_index === files.length - 1) {
+        if (index < files.length - 1) {
+          index++
+          readFile(files, index)
+        } else {
           fs.writeFileSync(`${rootPath}/README.md`, str, function(err){
             if (err) throw err
           })
@@ -37,4 +39,5 @@ files.forEach(function(filename, _index){
       }
     }
   })
-})
+}
+readFile(files, 0)
