725b9a6f0173c765dd6025113c9f26ffebeb1a46
tcly861204 <tcly861204@hotmail.com>
2019-05-19 21:55:32 +0800

    merge

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # blog
 个人创作
 
-
 [![Build Status](https://www.travis-ci.org/tcly861204/blog.svg?branch=master)](https://www.travis-ci.org/tcly861204/blog)
+
+[javascript设计模式之单例模式](https://tcly861204.github.io/2019/05/11/javascript设计模式之单例模式/)
+
+[javascript设计模式之适配器模式](https://tcly861204.github.io/2019/05/12/javascript设计模式之适配器模式/)
+
+[javscript高阶函数浅析](https://tcly861204.github.io/2019/05/12/javscript高阶函数浅析/)
+
+[vue组件间通信六种方式](https://tcly861204.github.io/2019/05/19/vue组件间通信六种方式/)
+
---
+++ cread/node.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const rootPath = path.resolve(__dirname, '..');
+const filePath = path.resolve(__dirname, '../source/_posts');
+let str = `# blog
+个人创作
+
+[![Build Status](https://www.travis-ci.org/tcly861204/blog.svg?branch=master)](https://www.travis-ci.org/tcly861204/blog)
+
+`
+const files = fs.readdirSync(filePath);
+files.forEach(function(filename, _index){
+  let filedir = path.join(filePath, filename);
+  fs.stat(filedir,function(eror, stats){
+    if (eror) {
+      console.warn('获取文件stats失败');
+    } else {
+      const isFile = stats.isFile();//是文件
+      // const isDir = stats.isDirectory();//是文件夹
+      if (isFile) {
+        let content = fs.readFileSync(filedir, 'utf-8');
+        let startIndexOf = content.indexOf('date: ')
+        if (startIndexOf > 0) {
+          let date = content.substr(startIndexOf + 6, 10)
+          let name = filename.replace('.md', '')
+          str += '[' + name + '](https://tcly861204.github.io/' + date.split('-').join('/') + '/' + name + '/)\r\n\r\n'
+        }
+        if (_index === files.length - 1) {
+          fs.writeFileSync(`${rootPath}/README.md`, str, function(err){
+            if (err) throw err
+          })
+        }
+      }
+    }
+  })
+})
